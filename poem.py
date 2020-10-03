ce1db1cee7636cb994d99b1c31a900de391e9a3b
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-10-02 19:04:54 -0700

    Made random function which prints random lines of the poem

--- poem.py
+++ poem.py
@@ -5,9 +5,21 @@ def get_file_lines(filename):
     # print(f.read().splitlines())
     return f.read().splitlines()
 lines_list = get_file_lines("poem.txt")
-# print(lines_list)
+#print(lines_list)
 
 def lines_printed_backwards(lines_list):
+     for i in range(len(lines_list)):
+         print(i, lines_list[-(i + 1)])
+#lines_printed_backwards(lines_list)
+
+def lines_printed_random(lines_list):
     for i in range(len(lines_list)):
-        print(i, lines_list[-(i + 1)])
-lines_printed_backwards(lines_list)
+         print(i, lines_list[random.randint(0, 13)])
+#lines_printed_random(lines_list)
+
+def lines_printed_customs(lines_list):
+    for i in range(len(lines_list)):
+         print(i, lines_list, random.randint(0, 5))
+         print(i, lines_list[6,13])
+lines_printed_customs(lines_list)
+         
